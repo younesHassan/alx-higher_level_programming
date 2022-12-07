71a8b8a3849d54a645e84502971e107f84cad94c
younesHassan <105860222+younesHassan@users.noreply.github.com>
2022-12-08 00:47:19 +0300

    Create 103-fast_alphabet.py

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the alphabet in uppercase, followed by a new line
+# -----------------------------------------------------------
+
+# Import the string module
+import string
+
+# Print out alphabets
+print(string.ascii_uppercase)
