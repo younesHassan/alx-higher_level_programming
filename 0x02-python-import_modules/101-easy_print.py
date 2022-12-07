00eb3bf10ad4051fa69f06b85e5a2972d6e19a44
younesHassan <105860222+younesHassan@users.noreply.github.com>
2022-12-08 00:46:01 +0300

    Create 101-easy_print.py

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print #pythoniscool, followed by a new line,
+# in the standard output.
+# -----------------------------------------------------------
+
+exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
