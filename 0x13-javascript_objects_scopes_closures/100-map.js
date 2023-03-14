bdde38b18f6c8793be61e126aa84303e59be5c01
younesHassan <105860222+younesHassan@users.noreply.github.com>
2023-03-15 01:24:38 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
 
-console.log(list);
+exports.list = [1, 2, 3, 4, 5];
