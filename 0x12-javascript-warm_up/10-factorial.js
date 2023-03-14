64d96ff60f8b4166b42afc48214ff24aaea3694c
younesHassan <105860222+younesHassan@users.noreply.github.com>
2023-03-15 01:32:59 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-// computes and prints a factorial
-
 function factorial (n) {
-  if ((isNaN(n)) || (n === 1)) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+    if (isNaN(n) || n === 0) {
+	return 1;
+    }   else {
+	return n * factorial(n - 1);
+    }
 }
-
 console.log(factorial(parseInt(process.argv[2])));
