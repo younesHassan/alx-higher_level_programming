db08ea2a8ba4ff6f5bb3bd65b03c532b258247d2
younesHassan <105860222+younesHassan@users.noreply.github.com>
2023-03-15 01:16:29 +0300

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-class Rectangle {
+
+module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
+
+    this.width = w;
+
+    this.height = h;
   }
-}
+};
