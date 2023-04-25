294d8680a72014a990188bb6f6f8fba16e813f2c
younesHassan <105860222+younesHassan@users.noreply.github.com>
2023-04-26 02:33:04 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
+// Gets the contents of a webpage and stores it in a file
 
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
+const file = process.argv[3];
 
-request(url, function (err, response, body) {
+request(url, function (err, res, body) {
   if (err) {
     console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
   }
+  fs.writeFile(file, body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
